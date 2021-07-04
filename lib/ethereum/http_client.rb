00d39aa0940bc1a75a3b6cd32592a172eefd183d
SFzxc <hoangphuocvanlong@gmail.com>
2021-07-05 02:38:20 +0700

    fix: support add custom request header

--- lib/ethereum/http_client.rb
+++ lib/ethereum/http_client.rb
@@ -1,24 +1,29 @@
 require 'net/http'
 require 'json'
+
 module Ethereum
   class HttpClient < Client
-    attr_accessor :host, :port, :uri, :ssl, :proxy
+    attr_accessor :host, :user, :pass, :port, :uri, :ssl, :proxy, :headers
 
-    def initialize(host, proxy = nil, log = false)
+    def initialize(host, proxy = nil, log = false, headers={})
       super(log)
       uri = URI.parse(host)
       raise ArgumentError unless ['http', 'https'].include? uri.scheme
       @host = uri.host
+      @user = uri.user
+      @pass = uri.password
       @port = uri.port
       @proxy = proxy
+      @headers = headers
 
       @ssl = uri.scheme == 'https'
-      @uri = URI("#{uri.scheme}://#{@host}:#{@port}#{uri.path}")
+      @uri = URI("#{uri.scheme}://#{@user}:#{@pass}@#{@host}:#{@port}#{uri.path}")
     end
 
     def send_single(payload)
       client = RestClient::Resource.new(uri.to_s)
-      client.post(payload, accept: 'json', content_type: 'json' )
+      @headers.merge!({ accept: 'json', content_type: 'json' })
+      client.post(payload, @headers)
     end
 
     def send_batch(batch)
